040113e0c1447938c914417ecd199f8c8feb9d0d
Touch <chantouchsek.cs83@gmail.com>
2024-05-06 13:25:33 +0700

    chore: add km locale

--- src/resources/lang/km/criteria.php
+++ src/resources/lang/km/criteria.php
@@ -1,4 +1,4 @@
 <?php
 return [
-    'fields_not_accepted' => 'Columns :field are not accepted in the research'
+    'fields_not_accepted' => 'ជួរឈរ :field មិនត្រូវបានទទួលយកក្នុងការស្រាវជ្រាវ។',
 ];
